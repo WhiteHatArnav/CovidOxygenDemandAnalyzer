baf41295549d0b9c3c0bec2b694ac15d3a45f975
Arnav Joshi <arnavjsh12@gmail.com>
2021-04-27 02:33:38 -0400

    Create OxygenDemandPredictor

---
+++ OxygenDemandPredictor.py
@@ -0,0 +1,106 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
+
+nActiveCasesPeak = 4800000
+nCurrentActiveCases = 2813658
+presentNewCases = 350000
+PeakNewCases = 460000
+nCurrentNewActiveCases = 130900
+durationToPeak =  20 #days
+RecoveryRatio = nCurrentNewActiveCases/presentNewCases
+
+
+VolOxyReqPerson = 550
+rOxSup = 0.04
+rLtoTn = 0.00114
+
+
+print(RecoveryRatio)
+TotalActiveCaseList = [0]*durationToPeak
+TotalActiveCaseList[0] = nCurrentActiveCases
+TotalActiveCaseList[19] = nActiveCasesPeak
+
+DailyAcceleration =  ((PeakNewCases - presentNewCases)/10)
+IndexList = list(range(1,21))
+IndexList1 = [0]*10
+IndexList1[0:9] = list(range(1,11))
+IndexList1[10] = 20
+
+
+newCaselist = [0] * 20
+newCaselist[0] = presentNewCases
+
+for i in range(1,10):
+    newCaselist[i] = (int)(newCaselist[i-1] + DailyAcceleration)
+
+for j in range(1,10):
+    TotalActiveCaseList[j] = (int)(TotalActiveCaseList[j-1] + RecoveryRatio*newCaselist[j])
+
+
+RegressList = [0]*11
+RegressList[0:10] = TotalActiveCaseList[0:10]
+RegressList[10] = TotalActiveCaseList[19]
+
+
+PolyModel = np.poly1d(np.polyfit(IndexList1,RegressList, 4))
+PolyLine = np.linspace(1, 20, nActiveCasesPeak)
+
+r2 = r2_score(RegressList, PolyModel(IndexList1))
+
+
+for k in range(10,20):
+    TotalActiveCaseList[k] = int(PolyModel(k+1))
+    
+for a in range(10,20):    
+    newCaselist[a] = (int)((TotalActiveCaseList[a] - TotalActiveCaseList[a-1])/RecoveryRatio)
+    if newCaselist[a] <= 0:
+        newCaselist[a] = newCaselist[a-1]
+
+
+
+
+massOxSupNeed = [0]*20
+for p in range(0,20):
+    nDailyOxSupNeed = int(rOxSup * newCaselist[p])
+    massOxSupNeed[p] = nDailyOxSupNeed * VolOxyReqPerson * rLtoTn
+
+print("\nR Squared Value for cubic polynomial best fit curve for Oxygen Demand in India April 26th through May 16th:")
+print("{:.6f}".format(r2))
+
+
+OxyModel = np.poly1d(np.polyfit(range(1,18),massOxSupNeed[0:17], 3))
+OxyLine = np.linspace(1, 17, 100)
+
+OxyR2 = r2_score(massOxSupNeed[0:17], OxyModel(range(1,18)))
+
+print("R Squared Value for quartic polynomial regression of Total Active Cases in India April 26th through May 16th:")
+print("{:.6f}".format(OxyR2))
+
+
+plt.scatter(range(1,18), massOxSupNeed[0:17])
+plt.plot(OxyLine,OxyModel(OxyLine))
+plt.title('Predictive Polynomial Best Fit Curve for Oxygen Demand in India till Peak in May, 2021')
+plt.xlabel('Days from April 26th, 2021')
+plt.ylabel('Oxygen Demand in India(Tonnes)')
+plt.xticks(np.arange(1, 18, 1))
+plt.show()
+
+plt.scatter(IndexList1,RegressList)
+plt.plot(PolyLine, PolyModel(PolyLine)) 
+plt.title('Predictive Polynomial Regression of Active Covid Cases in India')
+plt.xlabel('Days from April 26th, 2021')
+plt.ylabel('Projected Total Active Covid-19 Cases in India')
+plt.xticks(np.arange(min(IndexList1), max(IndexList1)+1, 1))
+plt.show()
+
+
+
+
+
+
+
+
+
+
